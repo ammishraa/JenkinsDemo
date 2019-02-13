80f40f5e28b85205ab1961069a026d8a7e2146f3
Ashutosh Mishra <amishra269@asiapac.globalcsc.net>
2019-02-13 20:12:56 +0530

    commited

---
+++ src/main/java/JenkinsDemo/JenkinsDemo/MainTest.java
@@ -0,0 +1,21 @@
+package JenkinsDemo.JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+	@Test
+	public void f1() {
+		System.out.println("1");
+	}
+
+	@Test
+	public void f2() {
+		System.out.println("2");
+	}
+
+	@Test
+	public void f3() {
+		System.out.println("3");
+	}
+}
